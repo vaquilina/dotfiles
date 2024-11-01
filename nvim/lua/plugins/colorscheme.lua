fc67ef6ae1e9db21aa2443d5362d78eb027cf0d2
Vince Aquilina <vince.aquilina@protonmail.com>
2024-11-01 17:34:19 -0400

    use transparent mode in colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
     config = function()
         require("gruvbox").setup({
             contrast = "hard",
-            transparent_mode = false,
+            transparent_mode = true,
         })
 
         vim.cmd([[colorscheme gruvbox]])
